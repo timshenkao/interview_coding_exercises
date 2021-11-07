143f5152af60980d72b9d1ac5b0b8be236c2faac
Tim Shenkao <timshenkao@gmail.com>
2021-11-07 12:14:19 -0800

    Solutions for exercises 112 226 572

---
+++ python/112_Path_Sum_easy/solution.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2021 - present, Timur Shenkao
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
+from typing import Optional
+from helper import TreeNode
+
+# 112. Path Sum  https://leetcode.com/problems/path-sum/
+# Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that
+# adding up all the values along the path equals targetSum.
+# A leaf is a node with no children.
+
+
+class Solution:
+    def _has_path_sum_recursion(self, root: Optional[TreeNode], target_sum, partial_sum) -> bool:
+        # the node is a leaf
+        if not root.left and not root.right:
+            return (partial_sum + root.val) == target_sum
+
+        # the node is NOT a leaf
+        if root.left:
+            is_left_path_sum = self._has_path_sum_recursion(root.left, target_sum, partial_sum + root.val)
+            # There is root-to-leaf path in left subtree. No need ton continue
+            if is_left_path_sum:
+                return is_left_path_sum
+        # there is no left child
+        else:
+            is_left_path_sum = False
+
+        # the node is NOT a leaf
+        if root.right:
+            is_right_path_sum = self._has_path_sum_recursion(root.right, target_sum, partial_sum + root.val)
+            # There is root-to-leaf path in right subtree. No need ton continue
+            if is_right_path_sum:
+                return is_right_path_sum
+        # there is no right child
+        else:
+            is_right_path_sum = False
+
+        # combine results from left and right subtrees
+        return is_left_path_sum or is_right_path_sum
+
+    def has_path_sum(self, root: Optional[TreeNode], target_sum: int) -> bool:
+        """ Time complexity: O(N). We check / visit every node
+            Space complexity: if we don't count recursion stack then O(1).
+                              Else O(log N) in case of balanced tree or O(N) in case of unbalanced tree.
+        """
+        # empty tree
+        if not root:
+            return False
+
+        # check paths via left child or right child
+        return self._has_path_sum_recursion(root, target_sum, 0)
---
+++ python/226_Invert_Binary_Tree_easy/helper.py
@@ -0,0 +1,78 @@
+# Copyright (c) 2021 - present, Timur Shenkao
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def _generate_binary_tree_recursion(values: List[int], root: Optional[TreeNode], current_index: int) -> \
+        Optional[TreeNode]:
+    length = len(values)
+
+    # create root node for the first call
+    if not root:
+        root = TreeNode(values[0], None, None)
+
+    # there is value for left child; don't generate node if value is None
+    if (2 * current_index + 1) <= length - 1 and values[2 * current_index + 1]:
+        root.left = TreeNode(values[2 * current_index + 1], None, None)
+        # call recursively; if left child has its own children, they will be populated
+        _generate_binary_tree_recursion(values, root.left, 2 * current_index + 1)
+
+    # there is value for right child; don't generate node if value is None
+    if (2 * current_index + 2) <= length - 1 and values[2 * current_index + 2]:
+        root.right = TreeNode(values[2 * current_index + 2], None, None)
+        # call recursively; if right child has its own children, they will be populated
+        _generate_binary_tree_recursion(values, root.right, 2 * current_index + 2)
+    return root
+
+
+def generate_binary_tree(values: List[int]) -> Optional[TreeNode]:
+    """ Generate binary tree from list of values.
+        Tree is not binary-search tree or balanced tree specifically.
+    """
+    # empty list --> no tree
+    if not values:
+        return None
+
+    current = 0
+    # call recursive function to generate tree
+    return _generate_binary_tree_recursion(values, None, current)
+
+
+def print_node(node: Optional[TreeNode]) -> None:
+    print('node: ', node, '; value: ', node.val, '; left : ', node.left, '; right : ', node.right)
+
+
+def print_binary_tree(root: Optional[TreeNode], is_root: bool = False) -> None:
+    if root:
+        if is_root:
+            print()
+        print_node(root)
+
+        if root.left:
+            print('left child of ', root, ' -->')
+            print_binary_tree(root.left)
+
+        if root.right:
+            print('right child of ', root, ' -->')
+            print_binary_tree(root.right)
